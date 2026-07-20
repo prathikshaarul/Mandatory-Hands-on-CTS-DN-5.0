1934e0aa43a7b4417168480887106357f08a526b
Prathiksha <prathiksha2468@gmail.com>
2026-07-20 10:16:08 +0530

    Added Angular hands-on output screenshots and updated routing

--- Angular Hands On/student-course-portal/src/app/app.ts
+++ Angular Hands On/student-course-portal/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
